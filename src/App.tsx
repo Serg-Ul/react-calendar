034912cd067dcd20a2928faaa212b13ff9dc2939
Serhii <serhii@gmail.com>
2023-04-08 22:40:55 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(): JSX {
+  return (
+    <div className="App">
+      <div onClick={event => event}>App</div>
+    </div>
+  );
+}
+
+export default App;
